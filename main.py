a12179380bdfdd7fb28a1fbe90b89a25db3f4677
Tomyiak <144472353+Tomyiak@users.noreply.github.com>
2023-09-09 11:05:05 +0300

    pogoda

--- main.py
+++ main.py
@@ -4,13 +4,23 @@ import requests
 
 city = "Moscow,RU"
 appid = "cb6612cc1dbf77152115c7e4e610a39a"
-print('huy')
+print('hu')
 res = requests.get("http://api.openweathermap.org/data/2.5/weather",
     params={'q': city, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
 data = res.json()
 
 print("Город:",city)
 print("Погодные условия:", data['weather'][0]['description'])
-print("Температура:", data)
-print("Минимальная температура:", data)
-print("Максимальная температура", data)
+print("Температура:", data['main']['temp'])
+print("Минимальная температура:", data['main']['temp_min'])
+print("Максимальная температура", data['main']['temp_max'])
+
+print()
+
+res = requests.get("http://api.openweathermap.org/data/2.5/forecast",
+params={'q': city, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
+data = res.json()
+print("Прогноз погоды на неделю:")
+for i in data['list']:
+    print("Дата <", i['dt_txt'], "> \r\nТемпература <", '{0:+3.0f}'.format(i['main']['temp']), "> \r\nПогодные условия <", i['weather'][0]['description'], ">")
+print("____________________________")
