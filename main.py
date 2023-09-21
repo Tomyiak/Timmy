7c549119ba3f23d97b32abcc5de26acf9927c5c4
Tomyiak <144472353+Tomyiak@users.noreply.github.com>
2023-09-21 23:09:54 +0300

    pogoda + perevod sistem

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import math
-import random
 import requests
 
 city = "Moscow,RU"
@@ -14,6 +12,8 @@ print("Погодные условия:", data['weather'][0]['description'])
 print("Температура:", data['main']['temp'])
 print("Минимальная температура:", data['main']['temp_min'])
 print("Максимальная температура", data['main']['temp_max'])
+print("Скорость ветра", data['wind']['speed'])
+print("Видимость", data['visibility'])
 
 print()
 
@@ -22,5 +22,9 @@ params={'q': city, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
 data = res.json()
 print("Прогноз погоды на неделю:")
 for i in data['list']:
-    print("Дата <", i['dt_txt'], "> \r\nТемпература <", '{0:+3.0f}'.format(i['main']['temp']), "> \r\nПогодные условия <", i['weather'][0]['description'], ">")
-    print("____________________________")
+    print("Дата <", i['dt_txt'], "> \r\nТемпература <", '{0:+3.0f}'.format(i['main']['temp']), "> \r\nПогодные условия <", i['weather'][0]['description'], ">\r\nСкорость ветра", i['wind']['speed'],
+          f'\r\nВидимость {i["visibility"]}')
+    print()
+    print()
+
+print(data['list'])
